248022f0a0cfe109d2e8c50371f02efdb461a2eb
dwcy2306 <dwcy2306@naver.com>
2018-01-11 17:12:37 +0900

    상보필터 적용

--- _180110-MPU9250_Filter_Test.ino
+++ _180110-MPU9250_Filter_Test.ino
@@ -26,8 +26,8 @@ float dt;
 float accel_angle_x, accel_angle_y, accel_angle_z;
 float gyro_angle_x, gyro_angle_y, gyro_angle_z;
 float filtered_angle_x, filtered_angle_y, filtered_angle_z;
-float baseAcX, baseAcY, baseAcZ;  //가속도 평균값 저장 변수
-float baseGyX, baseGyY, baseGyZ;  //자이로 평균값 저장 변수
+float baseAcX, baseAcY, baseAcZ;
+float baseGyX, baseGyY, baseGyZ;
 int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
 
 static float xyz_GyrAccMag[9];  // 0, 1, 2 = Acc, 3 = Mag, 4, 5, 6 = Gyr
@@ -112,8 +112,6 @@ void loop() {
   calcGyroYPR();
   calcFilteredYPR();
   SendDataToProcessing();
-  
-  delay(100);
 }
 
 void readAccelGyro() {
@@ -127,6 +125,7 @@ void readAccelGyro() {
 }
 
 void SendDataToProcessing() {
+  /*
   Serial.print(accel_angle_x, 2);  // acc
   Serial.print(",");
   Serial.print(accel_angle_y, 2);
@@ -139,10 +138,11 @@ void SendDataToProcessing() {
   Serial.print(",");
   Serial.print(gyro_angle_z, 2);
   Serial.print(",");
+  */
   Serial.print(filtered_angle_x, 2);  // fil
-  Serial.print(",");
+  Serial.print("\t");
   Serial.print(filtered_angle_y, 2);
-  Serial.print(",");
+  Serial.print("\t");
   Serial.print(filtered_angle_z, 2);
   Serial.print("\r\n");
 }
@@ -152,8 +152,7 @@ void calibAccelGyro() {
   float sumGyX = 0, sumGyY = 0, sumGyZ = 0;
 
   readAccelGyro();
-
-  //평균값 구하기
+  
   for(int i=0; i<10; i++){
     readAccelGyro();
     sumAcX += AcX; sumAcY += AcY; sumAcZ += AcZ;
@@ -164,8 +163,8 @@ void calibAccelGyro() {
   baseGyX = sumGyX / 10; baseGyY = sumGyY / 10; baseGyZ = sumGyZ / 10;
 }
 
-unsigned long t_now;  //현재 측정 주기 시간
-unsigned long t_prev; //이전 측정 주기 시간
+unsigned long t_now; 
+unsigned long t_prev;
 
 void initDT() {
   t_prev = millis();
@@ -173,22 +172,21 @@ void initDT() {
 
 void calcDT() {
   t_now = millis();
-  dt = (t_now - t_prev) / 1000.0; //millis()로 얻은 값은 밀리초 단위
+  dt = (t_now - t_prev) / 1000.0;
   t_prev = t_now;
 }
 
 void calcAccelYPR(){
-  float accel_x, accel_y, accel_z; //가속도 센서의 최종적인 보정값
+  float accel_x, accel_y, accel_z;
   float accel_xz, accel_yz;
   const float RADIANS_TO_DEGREES = 180/3.14159;
 
-  accel_x = AcX - baseAcX; // 가속도(직선) X축에 대한 현재 값 - 가속도 센서의 평균값
+  accel_x = AcX - baseAcX;
   accel_y = AcY - baseAcY;
   accel_z = AcZ + (16384 - baseAcZ);
 
-  //직석 +X축이 기울어진 각도 구함
   accel_yz = sqrt(pow(accel_y, 2) + pow(accel_z, 2));
-  accel_angle_y = atan(-accel_x / accel_yz)*RADIANS_TO_DEGREES;
+  accel_angle_y = atan(-accel_x / accel_yz)*RADIANS_TO_DEGREES;;
 
   accel_xz = sqrt(pow(accel_x, 2) + pow(accel_z, 2));
   accel_angle_x = atan(accel_y / accel_xz)*RADIANS_TO_DEGREES;
@@ -196,33 +194,38 @@ void calcAccelYPR(){
   accel_angle_z = 0;
 }
 
-float gyro_x, gyro_y, gyro_z; //각속도 저장 전역변수 //각속도 : 단위시간당 회전한 각도
+float gyro_x, gyro_y, gyro_z;
 
 void calcGyroYPR(){
   const float GYROXYZ_TO_DEGREES_PER_SED = 131;
-                                  //131 값은 1초동안 1도 돌때 자이로 값이 131이다.
-                                  
+
   gyro_x = (GyX - baseGyX) / GYROXYZ_TO_DEGREES_PER_SED;
   gyro_y = (GyY - baseGyY) / GYROXYZ_TO_DEGREES_PER_SED;
   gyro_z = (GyZ - baseGyZ) / GYROXYZ_TO_DEGREES_PER_SED;
 
-  gyro_angle_x += gyro_x * dt; //변화된 각 : 각속도 x 측정 주기 시간
+  gyro_angle_x += gyro_x * dt;
   gyro_angle_y += gyro_y * dt;
   gyro_angle_z += gyro_z * dt;
 }
 
 void calcFilteredYPR(){
-  const float ALPHA = 0.96;
-  float tmp_angle_x, tmp_angle_y, tmp_angle_z;  //이전 필터 각도(prev)
+  const float ALPHA = 0.98;
+  float tmp_angle_x, tmp_angle_y, tmp_angle_z;
 
-  tmp_angle_x = filtered_angle_x + gyro_x * dt; //자이로 각도 = 각속도 x 센서 입력 주기
-                                                //각속도 = 단위시간당 회전한 각도 -> 회전한 각도 / 단위시간
+  tmp_angle_x = filtered_angle_x + gyro_x * dt;
   tmp_angle_y = filtered_angle_y + gyro_y * dt;
   tmp_angle_z = filtered_angle_z + gyro_z * dt;
 
   filtered_angle_x = ALPHA * tmp_angle_x + (1.0 - ALPHA) * accel_angle_x;
   filtered_angle_y = ALPHA * tmp_angle_y + (1.0 - ALPHA) * accel_angle_y;
-  filtered_angle_z = tmp_angle_z; //곡선 +Z 축은 자이로 센서만 이용해서 나타내고 있음(가속도 센서는 안함)
-  //그래서 위에(calcAccelYPR) 보게 되면 accel_angle_z 는 0이다.
+  filtered_angle_z = tmp_angle_z;
 }
+
+
+
+
+
+
+
+
 
